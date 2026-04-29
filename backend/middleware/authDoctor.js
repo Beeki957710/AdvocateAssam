0055daadc656a8df20b1b921714e68f53e5b2748
Beeki957710 <dbishal536@gmail.com>
2026-04-30 00:49:28 +0530

    Initial commit: doctor appointment system

---
+++ backend/middleware/authDoctor.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+// doctor authentication middleware
+const authDoctor = async (req, res, next) => {
+  try {
+    const dtoken = req.headers["dtoken"];
+
+    if (!dtoken) {
+      return res.json({
+        success: false,
+        message: "Not Authorized Login Again",
+      });
+    }
+    const token_decode = jwt.verify(dtoken, process.env.JWT_SECRET);
+    
+    req.user = token_decode; // ✅ FIX      // **********req.body.userId = token_decode.id***********//
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export default authDoctor;
