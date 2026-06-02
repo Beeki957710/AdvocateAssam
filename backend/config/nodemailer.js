c539c9d6d8788db29c8bb77b806aa777c6d8a810
Beeki957710 <dbishal536@gmail.com>
2026-06-03 01:03:02 +0530

    Debug Brevo env

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -16,9 +16,9 @@
 // export default transporter;
 
 import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-dotenv.config();
 
+console.log("BREVO_USER =", process.env.BREVO_USER);
+console.log("BREVO_PASS =", process.env.BREVO_PASS ? "Loaded" : "Missing");
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
