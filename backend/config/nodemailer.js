058401869cafb594d3ffc5baee86c7a5aa09299c
Beeki957710 <dbishal536@gmail.com>
2026-06-02 22:53:55 +0530

    Fix email  notifications

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,12 +1,25 @@
+// import nodemailer from "nodemailer";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// export default transporter;
+
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
-dotenv.config();
-
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
