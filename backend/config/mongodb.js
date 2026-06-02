fce7a99097d9de1a545330df72bff6a3e71bcd33
Beeki957710 <dbishal536@gmail.com>
2026-06-02 15:36:24 +0530

    Change database name to AdvocateAssam

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,6 +4,6 @@ const connectDB = async () => {
 
     mongoose.connection.on('connected', () => console.log("Database Connected"))
     
-    await mongoose.connect(`${process.env.MONGODB_URI}/nyaysetu`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/AdvocateAssam`)
 }
 export default connectDB
